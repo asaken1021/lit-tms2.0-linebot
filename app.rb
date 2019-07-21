16218a04d7606df27388a2c254ae85ae291c48f0
asaken1021 <asaken1021@outlook.com>
2019-07-22 00:26:25 +0900

    pa

--- app.rb
+++ app.rb
@@ -56,7 +56,7 @@ post '/webhook' do
         if event.message['text'] = '連携'
           userID = event['source']['userId']
           linkTokenResponse = client.create_link_token(userID)
-          logger.info linkTokenResponse.body['linkToken']
+          logger.info JSON.parse(linkTokenResponse.body)
           linkToken = linkTokenResponse
           message = {
             type: 'text',
