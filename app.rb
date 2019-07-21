6e44e82aa6f81b665560be23ad9f6d5c99da9536
asaken1021 <asaken1021@outlook.com>
2019-07-21 15:27:50 +0900

    修正

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get '/' do
 end
 
 def client
-  @client || = Line::Bot::Client.new {|config|
+  @client ||= Line::Bot::Client.new {|config|
     config.channel_secret = ENV['LINE_CHANNEL_SECRET']
     config.channel_token = ENV['LINE_CHANNEL_TOKEN']
   }
