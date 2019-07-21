eceb2df73f85dca8944f0c3aacb36e9915f13fe4
asaken1021 <asaken1021@outlook.com>
2019-07-21 16:49:30 +0900

    ftghvjkl

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ post '/send_notify' do
     text: params[:message]
   }
   client.push_message(params[:to], message)
-  logger.info request.body
+  logger.info request.body.read
   logger.info params[:to]
   logger.info message
 end
