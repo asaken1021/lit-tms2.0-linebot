17fe9c7493ade858ad24bc5b67882292dcd253ec
asaken1021 <asaken1021@outlook.com>
2019-07-21 18:18:11 +0900

    e

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ end
 post '/send_notify' do
   request.body.rewind
   logger.info request.body.read
-  params = JSON.parse request.body.read
+  params = JSON.parse(request.body.read.to_json)
   message = {
     type: 'text',
     text: params[:message]
